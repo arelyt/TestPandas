8f268bbe3826ac1bc428d202c39c34d2aff1ecfc
arelyt <arelyt26@gmail.com>
2016-07-24 13:57:07 +0300

    1st text new

--- Test_1.py
+++ Test_1.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
+import datetime as dt
 import numpy
 import pandas as pd
 from pylab import *
 
-bhp = pd.read_csv('BHP.csv', delimiter=',', usecols=(6,), unpack=True)
+#dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d %H:%M:%S.%MS')
 
-bhp_returns = numpy.diff(bhp) / bhp[: -1]
+def parse(x):
+    return dt.datetime.strptime(x, '%Y%m%d %H:%M:%S %f')
 
-vale = pd.read_csv('VALE.csv', delimiter=',', usecols=(6,), unpack=True)
+bhp = pd.read_csv('RIM6_4.trd', usecols=(0, 1, 2, 3, 4, 5, 6),
+                  parse_dates={'DateTime': [0, 1]},
+                  index_col=0)
+print bhp
+print type(bhp.index)
+print bhp.index
+#sec = bhp['<DATE>_<TIME>_<MSEC>'].to_datetime('<DATE>_<TIME>_<MSEC>')
+#print sec
+#bhp_returns = numpy.diff(bhp) / bhp[: -1]
 
-vale_returns = numpy.diff(vale) / vale[: -1]
+# vale = pd.read_csv('VALE.csv', delimiter=',', usecols=(6,), unpack=True)
+
+# vale_returns = numpy.diff(vale) / vale[: -1]
