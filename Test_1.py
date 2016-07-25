72ec7f1165a63e0094bd74f3128b7588de4b6dfa
arelyt <arelyt26@gmail.com>
2016-07-25 09:33:13 +0300

    1st working!..

--- Test_1.py
+++ Test_1.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+import matplotlib.pyplot as plt
+from matplotlib.finance import candlestick_ohlc, candlestick2_ohlc
+import matplotlib.dates as mdates
 import datetime as dt
 import numpy
 import pandas as pd
+#matplotlib.style.use('ggplot')
+
 from pylab import *
 
 #dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d %H:%M:%S.%MS')
@@ -15,6 +20,29 @@ bhp = pd.read_csv('RIM6_4.trd', usecols=(0, 1, 2, 3, 4, 5, 6),
 print bhp
 print type(bhp.index)
 print bhp.index
+print bhp.columns
+bhp = bhp.rename(columns={'<LAST>': 'Price', '<VOL>': 'Volume'})
+print bhp
+res = bhp.Price.resample('1s').ohlc()
+print res
+res.reset_index(inplace=True)
+#res.DateTime = mdates.date2num(res.DateTime)
+dataAr = [tuple(x) for x in res[['DateTime', 'open', 'high', 'low', 'close']].to_records(index=False)]
+plt.ion()
+#res = res.cumsum()
+#plt.figure()
+#res.plot()
+fig = plt.figure()
+ax1 = plt.subplot(1, 1, 1)
+candlestick_ohlc(ax1, dataAr)
+plt.show()
+print 1
+#print type(res.index)
+#print res.index
+#print res.columns
+#fig, ax = plt.subplots()
+#csticks = candlestick_ohlc(ax, res[['<LAST>'][0, 1, 2, 3]].values)
+#plt.show()
 #sec = bhp['<DATE>_<TIME>_<MSEC>'].to_datetime('<DATE>_<TIME>_<MSEC>')
 #print sec
 #bhp_returns = numpy.diff(bhp) / bhp[: -1]
