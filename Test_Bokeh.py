8caa590cefbd415beaa430716b573aff8aff0b64
arelyt <arelyt26@gmail.com>
2016-07-28 15:56:25 +0300

    First big graph with TickSpeed

--- Test_Bokeh.py
+++ Test_Bokeh.py
@@ -1,13 +1,6 @@
 from math import pi
-import datetime as dt
 import pandas as pd
-from bokeh.plotting import figure, output_notebook, show
-import numpy as np
-output_notebook()
-
-
-def parse(x):
-    return dt.datetime.strptime(x, '%Y%m%d %H:%M:%S')
+from bokeh.plotting import figure, output_file, show
 
 
 bhp = pd.read_csv('RIM6_4.trd', usecols=(0, 1, 4, 5, 6), header=0, parse_dates={'DateTime': [0, 1]}, index_col=[0])
@@ -22,6 +15,8 @@ resSell = bhp.VolSell.resample('60s').sum()
 resBuy = bhp.VolBuy.resample('60s').sum()
 
 res_new = res.join(pd.DataFrame(resSell))
+print res_new
+
 res_new1 = res_new.join(pd.DataFrame(resBuy))
 
 print res_new1
@@ -34,10 +29,13 @@ dec = res.open > res.close
 
 w = 12*60*60
 
-p = figure(x_axis_type='datetime', plot_width=800, plot_height=500, title='RIM6 CandleStick')
+p = figure(x_axis_type='datetime', plot_width=1024, plot_height=800, title='RIM6 CandleStick')
 p.xaxis.major_label_orientation = pi/4
 p.grid.grid_line_alpha = 0.3
-p.segment(res.DateTime, res.high, res.DateTime, res.low, color='black')
-p.rect(res.DateTime[inc], mids[inc], w, spans[inc], fill_color='#6CF350', line_color='green')
-p.rect(res.DateTime[dec], mids[dec], w, spans[dec], fill_color='#FF0000', line_color='red')
+p.segment(res.index, res.high, res.index, res.low, color='black')
+p.rect(res.index[inc], mids[inc], w, spans[inc], fill_color='#6CF350', line_color='green')
+p.rect(res.index[dec], mids[dec], w, spans[dec], fill_color='#FF0000', line_color='red')
+
+output_file('candlestick.html', title='RIM_4')
+
 show(p)
