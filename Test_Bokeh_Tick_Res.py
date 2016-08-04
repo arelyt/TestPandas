6b85883fa2206ad0122e4b8471f6f8aa33acb4c6
arelyt <arelyt26@gmail.com>
2016-08-04 17:11:34 +0300

    Add OTO

--- Test_Bokeh_Tick_Res.py
+++ Test_Bokeh_Tick_Res.py
@@ -4,8 +4,9 @@ from math import pi
 import datetime as dt
 import pandas as pd
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import LinearAxis, Range1d
+from bokeh.models import LogAxis, LinearAxis, Range1d
 # output_notebook()
+
 
 tick = pd.read_csv('RIM6_4.trd', usecols=(0, 1, 2, 3, 4, 5, 6),
                    parse_dates={'DateTime': [0,1]}, index_col=False)
@@ -39,8 +40,12 @@ for i in range(0, len(tick.index), Nticks):
     rows_list.append(dict1)
 
 res = pd.DataFrame(rows_list)
-res['Dsec'] = res['DeltaTime'].dt.total_seconds()
-# res = res.set_index('DateTime')
+res['Dsec']=res['DeltaTime'].dt.total_seconds()
+res['TSpeedBuy']=res['TickBuy']/res['Dsec']
+res['TSpeedSell']=res['TickSell']/res['Dsec']
+res['VSpeedBuy']=res['VolBuy']/res['Dsec']
+res['VSpeedSell']=res['VolSell']/res['Dsec']
+
 mids = (res.open + res.close)/2
 spans = abs(res.close - res.open)
 # print res, mids, spans
@@ -49,16 +54,23 @@ dec = res.open > res.close
 
 w = 0.8
 print res.index[inc], mids, spans, inc, dec
-p = figure(x_range=(0, len(res.index)), y_range=(90500, 95000), plot_width=1024, plot_height=800, title='RIM6 CandleStick')
+p = figure(x_range=(0, len(res.index)), plot_width=800, plot_height=600, title='RIM6 CandleStick')
+p.y_range=Range1d(start=90500, end=95000)
 p.xaxis.major_label_orientation = pi/4
 p.grid.grid_line_alpha = 0.3
 p.segment(res.index.values, res.high, res.index.values, res.low, color='black')
 p.rect(res.index[inc], mids[inc], w, spans[inc], fill_color='#6CF350', line_color='green')
 p.rect(res.index[dec], mids[dec], w, spans[dec], fill_color='#FF0000', line_color='red')
-p.extra_y_ranges = {"TickSpeed": Range1d(start=0, end=50)}
-p.line(res.index, res.TickSell/res.Dsec, color='blue', y_range_name='TickSpeed', alpha=0.5)
-p.line(res.index, res.TickBuy/res.Dsec, color='red', y_range_name='TickSpeed', alpha=0.5)
-p.add_layout(LinearAxis(y_range_name='TickSpeed'), 'left')
+p.extra_y_ranges = {}
+p.extra_y_ranges['TickSpeed'] = Range1d(start=0, end=100)
+p.extra_y_ranges['OTO'] = Range1d(start=-60, end=60)
+#p.line(res.index, res.TSpeedBuy, color='blue', y_range_name='TickSpeed', alpha=0.5)
+p.line(res.index, res.TSpeedBuy.rolling(window=4).median(), color='green', y_range_name='TickSpeed', alpha=0.8)
+#p.line(res.index, res.TSpeedSell, color='red', y_range_name='TickSpeed', alpha=0.5)
+p.line(res.index, res.TSpeedSell.rolling(window=9).median(), color='red', y_range_name='TickSpeed', alpha=0.8)
+p.line(res.index, res.OTO.rolling(window=15).mean(), color='brown', y_range_name='OTO', alpha=0.9)
+p.add_layout(LogAxis(y_range_name='TickSpeed'), 'left')
+p.add_layout(LinearAxis(y_range_name='OTO'), 'left')
 
 output_file('Tick_resample_candlestick.html', title='RIM_4')
 show(p)
