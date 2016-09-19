aec5cdd840464b23f85077ec17c92de1a6acdb65
arelyt <arelyt26@gmail.com>
2016-09-19 11:03:41 +0300

    !!! Deltas work! in Matlab1

--- TimeOHLC.m
+++ TimeOHLC.m
@@ -7,18 +7,28 @@ x = table.AbsCumDelta(1:deltas:end);
 y = table.Price(1:deltas:end);
 
 %Цикл по OHLC
-opn = zeros(1, int64(max(table.AbsCumDelta)/deltas));
-high = zeros(1, int64(max(table.AbsCumDelta)/deltas));
-low = zeros(1, int64(max(table.AbsCumDelta)/deltas));
-close = zeros(1, int64(max(table.AbsCumDelta)/deltas));
+op = zeros(1, int64(max(table.AbsCumDelta)/deltas));
+hi = zeros(1, int64(max(table.AbsCumDelta)/deltas));
+lo = zeros(1, int64(max(table.AbsCumDelta)/deltas));
+cl = zeros(1, int64(max(table.AbsCumDelta)/deltas));
+da = repmat(datetime(0, 0, 0, 0, 0, 0), 1, int64(max(table.AbsCumDelta)/deltas));
 
 for i = 1:int64(max(table.AbsCumDelta)/deltas)
     ss = (i-1)*deltas;      % Левая граница выбора
     sd = deltas*i;           % Правая граница выбора
-    hi(i) = max(table.Price(table.AbsCumDelta>=ss & table.AbsCumDelta<sd));
-    lo(i) = min(table.Price(table.AbsCumDelta>=ss & table.AbsCumDelta<sd));
-    op(i) = find(table.Price(table.AbsCumDelta>=ss & table.AbsCumDelta<sd), table.Price, 'first');
-    cl(i) = find(table.Price(table.AbsCumDelta>=ss & table.AbsCumDelta<sd), table.Price, 'last');
+    t = table.Price(table.AbsCumDelta>=ss & table.AbsCumDelta<sd);
+    da(i) = max(table.DateTime(table.AbsCumDelta>=ss & table.AbsCumDelta<sd));
+    hi(i) = max(t);
+    lo(i) = min(t);
+    op(i) = t(1);
+    cl(i) = t(end);
 end
-x1 = 1:int64(max(table.AbsCumDelta)/deltas);
-plot(x1, opn);
+    d = datenum(da');
+    h = hi';
+    l = lo';
+    o = op';
+    c = cl';
+    x1 = 1:int64(max(table.AbsCumDelta)/deltas);
+
+candle(h, l, c, o);
+
